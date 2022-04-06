e3f60d5e4fcd8e1f9441519ddc4edab8142a79a0
limich <limich@umich.edu>
2022-04-06 13:06:14 -0400

    finished map0

--- hadoop/inverted_index/map0.py
+++ hadoop/inverted_index/map0.py
@@ -1,1 +1,43 @@
+import re
+import sys
+
 """Map 0."""
+# this will open the file
+files = 1
+allDocs = []
+stopWords = []
+allWords = {}
+with open("/hadoop/inverted_index/stopwords.txt") as stopWordsFile:
+    stopWords = stopWordsFile.readlines()
+for line in sys.stdin:
+    # get doc id, text, title, and clean up, remove stop words
+    id, title, text = line.split(",")
+    fullDoc = title + " " + text
+    fullDoc = re.sub(r"[^a-zA-Z0-9 ]+", "", fullDoc)
+    fullDoc = fullDoc.casefold()
+    allDocs.append(fullDoc)
+    words = fullDoc.split() 
+    for word in words:
+        if word not in stopWords:
+            allWords[word] = id
+# dict = {}
+# matrix = []
+# docId = 0
+#     # for every word, we go through each document,
+#     # if the word is in the document, we append docid to dict
+# wordId = 0
+# for word in allWords:
+#     dict[word] = []
+#     #matrix[docId] = []
+#     for doc in allDocs:
+#         docWords = doc.split()
+#         if word in docWords:
+#             dict[word].append(docId)
+#             #matrix[docId].append(1)
+#         #else:
+#             #matrix[docId].append(0)
+#         docId += 1
+        
+# #print(dict)
+sys.stdout(allWords)
+        
--- hadoop/inverted_index/reduce0.py
+++ hadoop/inverted_index/reduce0.py
@@ -1,1 +1,32 @@
 """Reduce 0."""
+#!/usr/bin/env python3
+"""Word count reducer."""
+import sys
+import itertools
+
+
+def reduce_one_group(key, group):
+    """Reduce one group."""
+    docs = []
+    for line in group:
+        value = line.partition("\t")[2]
+        if value not in docs:
+            docs.append(value)
+        
+    len(docs)
+
+
+def keyfunc(line):
+    """Return the key from a TAB-delimited key-value pair."""
+    return line.partition("\t")[0]
+
+
+def main():
+    """Divide sorted lines into groups that share a key."""
+    with open()
+    for key, group in itertools.groupby(sys.stdin, keyfunc):
+        reduce_one_group(key, group)
+
+
+if __name__ == "__main__":
+    main()
